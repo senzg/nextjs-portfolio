ae7220ce67ab59f9d58b8b18689be5fc5c0529a0
Steven-Zeng <Steven.Zeng@skyscanner.net>
2024-12-18 17:53:35 +0800

    Modified Navbar, add Blog option

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -7,6 +7,10 @@ import MenuOverlay from "./MenuOverlay";
 
 const navLinks = [
   {
+    title: "Home",
+    path: "/",
+  },
+  {
     title: "About",
     path: "#about",
   },
@@ -17,6 +21,10 @@ const navLinks = [
   {
     title: "Contact",
     path: "#contact",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
   },
 ];
 
